221902859f6a68da689605e677be2cab5e538336
KVCobbs <kenyacobbs9001@gmail.com>
2021-10-26 14:09:34 +0000

    Idk

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 //1
 function countToTwenty(){
-  
+  for(let i = 1; i <= 20; i++) {
+      console.log(i)
+  }
 }
 
 //2
 function countFromTwenty(){
-  
+  for(let i = 20; i >= 1; i--){
+      console.log(i)
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(num){
+  for(let i = num; i <= 1; i++){
+      console.log(i)
+  }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(num){
+  for(let i = num; i >= 1; i--){
+      console.log(i)
+  }
 }
 
 //5
 function countEveryOdd(){
+    for(let i= 1; i <= 35; i++){
+        if (i === 0) {
+            
+        }
+    }
   
 }
+
+//countToTwenty()
+//countFromTwenty()
+//countToN(15)
+//countFromN(20)
+
